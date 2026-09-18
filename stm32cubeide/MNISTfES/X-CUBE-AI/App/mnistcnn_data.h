0fe39152f18d4db92d151d0f7f86530b70683e0c
Elouan MARRON <opensource@mrbrownfr.com>
2026-09-18 08:54:31 +0200

    feat: created STM32CubeIDE X-CUBE-AI project

---
+++ stm32cubeide/MNISTfES/X-CUBE-AI/App/mnistcnn_data.h
@@ -0,0 +1,81 @@
+/**
+  ******************************************************************************
+  * @file    mnistcnn_data.h
+  * @author  AST Embedded Analytics Research Platform
+  * @date    2026-09-18T08:51:24+0200
+  * @brief   AI Tool Automatic Code Generator for Embedded NN computing
+  ******************************************************************************
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  ******************************************************************************
+  */
+
+#ifndef MNISTCNN_DATA_H
+#define MNISTCNN_DATA_H
+
+#include "mnistcnn_config.h"
+#include "mnistcnn_data_params.h"
+
+AI_DEPRECATED
+#define AI_MNISTCNN_DATA_ACTIVATIONS(ptr_)  \
+  ai_mnistcnn_data_activations_buffer_get(AI_HANDLE_PTR(ptr_))
+
+AI_DEPRECATED
+#define AI_MNISTCNN_DATA_WEIGHTS(ptr_)  \
+  ai_mnistcnn_data_weights_buffer_get(AI_HANDLE_PTR(ptr_))
+
+
+AI_API_DECLARE_BEGIN
+
+
+extern const ai_u64 s_mnistcnn_weights_array_u64[3239];
+
+
+
+/*!
+ * @brief Get network activations buffer initialized struct.
+ * @ingroup mnistcnn_data
+ * @param[in] ptr a pointer to the activations array storage area
+ * @return an ai_buffer initialized struct
+ */
+AI_DEPRECATED
+AI_API_ENTRY
+ai_buffer ai_mnistcnn_data_activations_buffer_get(const ai_handle ptr);
+
+/*!
+ * @brief Get network weights buffer initialized struct.
+ * @ingroup mnistcnn_data
+ * @param[in] ptr a pointer to the weights array storage area
+ * @return an ai_buffer initialized struct
+ */
+AI_DEPRECATED
+AI_API_ENTRY
+ai_buffer ai_mnistcnn_data_weights_buffer_get(const ai_handle ptr);
+
+/*!
+ * @brief Get network weights array pointer as a handle ptr.
+ * @ingroup mnistcnn_data
+ * @return a ai_handle pointer to the weights array
+ */
+AI_DEPRECATED
+AI_API_ENTRY
+ai_handle ai_mnistcnn_data_weights_get(void);
+
+
+/*!
+ * @brief Get network params configuration data structure.
+ * @ingroup mnistcnn_data
+ * @return true if a valid configuration is present, false otherwise
+ */
+AI_API_ENTRY
+ai_bool ai_mnistcnn_data_params_get(ai_network_params* params);
+
+
+AI_API_DECLARE_END
+
+#endif /* MNISTCNN_DATA_H */
+
